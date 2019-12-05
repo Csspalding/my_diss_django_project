e59b66b7da06082cc4f2cd98ca99042c018072a5
Cassie Spalding <2140148s@student.gla.ac.uk>
2019-12-05 13:04:17 +0000

    add password update to MyProfile

--- cupcake_site/urls.py
+++ cupcake_site/urls.py
@@ -3,18 +3,17 @@ from cupcake_site import views
 #from cupcake_site.views import AboutView #always import seperately 
 #from cupcake_site.views import IndexView  
 
-"""created 01/08/19 by Cassie Spalding, this file handles urls that startwith cupcakesite/ """
+"""@author Cassie Spalding 2140148s dissertation project 2019 
+this file handles urls that startwith cupcakesite/ """
 
 """note regarding django registration redux, url patterns - see login.html and other templates in templates/registration dir"""
 
 app_name = 'cupcake_site'
 
 urlpatterns = [
-    #path('', views.index, name='index'), #this url points to the index view function
     path('', views.IndexView.as_view(), name='index'), 
     path('about/', views.AboutView.as_view(), name='about'),
-    #path('h/', views.h, name='h'),#carsole  todo place this in a template tag on About later
-    path('career/', views.career, name='career'),#carsole  todo place this in a template tag on About later
+    path('career/', views.career, name='career'),
     path('tools/', views.tools, name='tools'),
     path('tools/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('add_category/',views.CategoryCreateView.as_view(), name='add_category'),
--- posts/urls.py
+++ posts/urls.py
@@ -11,6 +11,7 @@ from posts import views
 #Anything that starts with details, the data for each post in the db,  will check ?,  the P parameter which is a Django automatic id which autoincrements in the datebase as each new post is added 
 
 #pattern name should be of the form appname_viewname
+
 app_name = 'posts'
 
 urlpatterns = [
@@ -18,10 +19,4 @@ urlpatterns = [
   path('', views.posts_index, name='posts_index'),  
   path('posts_create/', views.PostCreate.as_view(),name = 'posts_create'), 
   path('posts_detail/<int:pk>', views.PostDetail.as_view(), name='posts_detail'), 
-   #path('paste/<int:pk>', PasteDetail.as_view(), name='pastebin_paste_detail'),
 ]
-
-  #add comments to posts
-  #add likes/views to posts
-  #Add a search to the site
-  #TESTING
